b160833d167d0f48a115e495f5301be5943e048a
Paul Lederer <plederer93@gmail.com>
2019-09-01 22:18:48 -0500

    Recognizes matched Tiles and deletes them. Blocks Fall

--- CirnoJam/Assets/Scripts/Nine.Core/Board.cs
+++ CirnoJam/Assets/Scripts/Nine.Core/Board.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace Nine.Core
 {
@@ -9,8 +10,7 @@ namespace Nine.Core
         public const int COLUMN_HEIGHT = 13;
         public readonly float ScrollTime;
         public float ScrollProgress;
-
-
+        
         // Blocks are accessed by Blocks[y][x]
 		public Block[][] Blocks { get; set; }
 		public bool GameOver
@@ -36,7 +36,7 @@ namespace Nine.Core
             }
         }
 
-        public Board(float scrollTime)
+        public Board(float scrollTime) //Constructor
         {
             this.ScrollTime = scrollTime;
             this.Initialize();
@@ -115,11 +115,111 @@ namespace Nine.Core
 
 			Blocks[pointA.Y][pointA.X] = blockB;
 			Blocks[pointB.Y][pointB.X] = blockA;
+
+            processMatches();
 		}
 
 		public void Update(float deltaTime)
 		{
             ScrollProgress += deltaTime;
-		}
-	}
+            fillGaps();
+        }
+
+        public Block GetBlock((int X, int Y) position)
+        {
+            return Blocks[position.Y][position.X];
+        }
+
+        Block GetBlock(int x, int y)
+        {
+            if(x >= 0 && y >= 1 && x < ROW_WIDTH && y < COLUMN_HEIGHT)
+            {
+                return Blocks[y][x];
+            }
+
+            return null;
+        }
+
+        private void fillGap(int x, int y)
+        {
+            Swap((x, y), (x, y + 1));
+        }
+
+        private void fillGaps()
+        {
+            for (int y = 0; y < COLUMN_HEIGHT - 1; y++)
+            {
+                for (int x = 0; x < ROW_WIDTH; x++)
+                {
+                    if (Blocks[y][x] == null)
+                    {
+                        fillGap(x, y);
+                    }
+                }
+            }
+        }
+
+        private void removeBlock(Block block)
+        {
+            Blocks[block.Position.Y][block.Position.X] = null;            
+        }
+
+        private void processMatches()
+        {
+            // do whatever we want to do with matches -- delete blocks for now
+            var matchBlocks = GetMatchBlocks();
+
+            foreach(var block in matchBlocks)
+            {
+                removeBlock(block);
+            }
+        }
+ 
+        IEnumerable<Block> GetMatchBlocks()
+        {
+            var matchBlocks = new List<Block>();
+
+            for (int y = 1; y < COLUMN_HEIGHT; y++)
+            {
+                for (int x = 0; x < ROW_WIDTH; x++)
+                {
+                    var thisBlock = GetBlock(x, y);
+
+                    if(thisBlock == null)
+                    {
+                        continue;
+                    }
+
+                    var leftBlock = GetBlock(x - 1, y);
+                    var rightBlock = GetBlock(x + 1, y);
+                    var aboveBlock = GetBlock(x, y + 1);
+                    var belowBlock = GetBlock(x, y - 1);
+
+                    if (leftBlock != null && rightBlock != null && thisBlock.Type == leftBlock.Type && thisBlock.Type == rightBlock.Type)
+                    {
+                        // horizontal match
+                        // TODO: flag blocks as inactive/able to be deleted by typing
+
+                        // for now: flag them for deletion
+                        matchBlocks.Add(leftBlock);
+                        matchBlocks.Add(thisBlock);
+                        matchBlocks.Add(rightBlock);
+                    }
+
+                    if (aboveBlock != null && belowBlock != null && thisBlock.Type == aboveBlock.Type && thisBlock.Type == belowBlock.Type)
+                    {
+                        // vertical match
+                        // TODO: flag blocks as inactive/able to be deleted by typing
+
+                        // for now: flag them for deletion
+                        matchBlocks.Add(aboveBlock);
+                        matchBlocks.Add(thisBlock);
+                        matchBlocks.Add(belowBlock);
+                    }
+                }
+            }
+
+            return matchBlocks.Distinct();
+        }
+    }
 }
--- CirnoJam/Assets/Scripts/Nine.Unity/ViewBoard.cs
+++ CirnoJam/Assets/Scripts/Nine.Unity/ViewBoard.cs
@@ -61,6 +61,7 @@ public class ViewBoard : MonoBehaviour
             gameBoard.Swap(Cursor.GridPosition, (Cursor.GridPosition.X+1, Cursor.GridPosition.Y) );
         }
 
+        Cursor.transform.position = this.transform.position + new Vector3(Cursor.GridPosition.X + 1, Cursor.GridPosition.Y + 1 + (gameBoard.ScrollProgress % gameBoard.ScrollTime) / gameBoard.ScrollTime, 1);
     }
 
     // Update is called once per frame
@@ -71,8 +72,8 @@ public class ViewBoard : MonoBehaviour
             return;
         }
         updateCursor();
-
         gameBoard.Update(Time.deltaTime);
+
 
         while (gameBoard.ScrollProgress >= gameBoard.ScrollTime)
         {
@@ -84,16 +85,42 @@ public class ViewBoard : MonoBehaviour
                     ViewBlockFactory.CreateBlock(gameBoard.Blocks[0][x], this.transform.position + new Vector3(x + 1, 1, 1), this.transform.rotation)
                 );
             }
+
             Cursor.Move(0, 1);
-            
+        }
+
+        sync();
+
+        IsGameOver = gameBoard.GameOver;
+    }
+
+    void sync()
+    {
+        foreach (var block in blocks)
+        {
+            GameObject.Destroy(block.gameObject);
+        }
+
+        blocks = new List<ViewBlock>();
+
+        for (int x = 0; x < Nine.Core.Board.ROW_WIDTH; x++)
+        {
+            for (int y = 0; y < Nine.Core.Board.COLUMN_HEIGHT; y++)
+            {
+                if (gameBoard.Blocks[y][x] != null)
+                {
+                    blocks.Add
+                    (
+                        ViewBlockFactory.CreateBlock(gameBoard.Blocks[y][x], this.transform.position + new Vector3(x + 1, y + 1, 1), this.transform.rotation)
+                    );
+                }
+            }
         }
 
         foreach (var block in blocks)
         {
             block.transform.position = this.transform.position + new Vector3(block.Block.Position.X + 1, block.Block.Position.Y + 1 + (gameBoard.ScrollProgress % gameBoard.ScrollTime) / gameBoard.ScrollTime, 1);
         }
-        Cursor.transform.position = this.transform.position + new Vector3(Cursor.GridPosition.X + 1, Cursor.GridPosition.Y + 1 + (gameBoard.ScrollProgress % gameBoard.ScrollTime) / gameBoard.ScrollTime, 1);
-
-        IsGameOver = gameBoard.GameOver;
     }
 }
+
