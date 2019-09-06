1d8b7d87ffc0633f5fea115dfb71ce6b050fca49
Paul Lederer <plederer93@gmail.com>
2019-09-05 21:59:07 -0500

    made UI work better

--- CirnoJam/Assets/Scripts/Nine.Unity/Scoreboard.cs
+++ CirnoJam/Assets/Scripts/Nine.Unity/Scoreboard.cs
@@ -9,14 +9,14 @@ public class Scoreboard : MonoBehaviour
 	public Button RestartButton;
 	public Button MainMenuButton;
 	public Button QuitButton;
+	public RawImage GameoverBackground;
 	private ViewBoard ViewBoard;
 	private bool gameover = false;
 
 	// Start is called before the first frame update
 	void Start()
     {
-		ScoreboardText = Instantiate(ScoreboardText, new Vector3(1400, 900, -50), this.transform.rotation);
-		ScoreboardText.transform.SetParent(this.transform, true);
+
 	}
 
     // Update is called once per frame
@@ -24,14 +24,12 @@ public class Scoreboard : MonoBehaviour
     {
 		ViewBoard = FindObjectOfType<ViewBoard>();
 		ScoreboardText.text = $"Score: { ViewBoard.Score }\nBlocks Cleared: { ViewBoard.BlocksCleared }";
-		if(ViewBoard.IsGameOver && !gameover) 
+		if(ViewBoard.IsGameOver && !gameover)
 		{
-			RestartButton = Instantiate(RestartButton, new Vector3(150, 900, -50), this.transform.rotation);
-			RestartButton.transform.SetParent(this.transform, true);
-			MainMenuButton = Instantiate(MainMenuButton, new Vector3(150, 850, -50), this.transform.rotation);
-			MainMenuButton.transform.SetParent(this.transform, true);
-			QuitButton = Instantiate(QuitButton, new Vector3(150, 800, -50), this.transform.rotation);
-			QuitButton.transform.SetParent(this.transform, true);
+			RestartButton.gameObject.SetActive(true);
+			MainMenuButton.gameObject.SetActive(true);
+			QuitButton.gameObject.SetActive(true);
+			GameoverBackground.gameObject.SetActive(true);
 			gameover = true;
 		}
 	}
